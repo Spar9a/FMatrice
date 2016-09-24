0e81cf9a1cf718b2dbac8e3fe40b8b1654835f6d
Sparda Dante <spar9a@gmail.com>
2016-09-24 21:57:21 +0300

    .

--- ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -11,10 +11,10 @@ void check()																									// Начало функции проверки
 	cin.clear();																								// если это не так, очищаем ввод
 	cin.ignore();																								// игнорируем все символы
 	cout << "Недопустимое значение!" << endl << "Попробуйте заново: ";
-}   
+}
 void inputA()
 {
-	cout << "Столбцы: ";
+	cout << "Строки: ";
 	while (!(cin >> string_x)) {
 		cin.clear();																							//если это не так, очищаем ввод
 		cin.ignore();																							//игнорируем все символы до знака '\string_x'
@@ -22,26 +22,26 @@ void inputA()
 	}
 	while (string_x < 1 || string_x > 100)																		// Цикл заставляющий ввести значения не меньше 1 и не больше 100
 	{
-		cout << "Недопустимая размерность матрицы, повтори ввод ещё раз" << endl << "Кол-во столбцов" << endl;
+		cout << "Недопустимая размерность матрицы, повтори ввод ещё раз" << endl << "Кол-во строк" << endl;
 		cin >> string_x;
 	}
-																												//Конец проверки вводимых символов
-	cout << "Строки: ";
+	//Конец проверки вводимых символов
+	cout << "Столбцы: ";
 	while (!(cin >> column_y)) {
-		cin.clear();											
-		cin.ignore();	
-		cout << "Недопустимое значение!" << endl << "Попробуйте заново: ";   
+		cin.clear();
+		cin.ignore();
+		cout << "Недопустимое значение!" << endl << "Попробуйте заново: ";
 	}
 	while (column_y < 1 || column_y > 100)																		// Цикл заставляющий ввести значения не меньше 1 и не больше 100
 	{
 		cout << "Недопустимая размерность матрицы, повтори ввод ещё раз" << endl;
-		cout << "Кол-во строк" << endl;
+		cout << "Кол-во столбцов" << endl;
 		cin >> column_y;
 	}
 }
 void inputB()
 {
-	cout << "Столбцы: ";
+	cout << "Строки: ";
 	while (!(cin >> string_n)) {
 		cin.clear();																							//если это не так, очищаем ввод
 		cin.ignore();																							//игнорируем все символы до знака '\string_x'
@@ -49,11 +49,11 @@ void inputB()
 	}
 	while (string_n < 1 || string_n > 100)																		// Цикл заставляющий ввести значения не меньше 1 и не больше 100
 	{
-		cout << "Недопустимая размерность матрицы, повтори ввод ещё раз" << endl << "Кол-во столбцов" << endl;
+		cout << "Недопустимая размерность матрицы, повтори ввод ещё раз" << endl << "Кол-во строк" << endl;
 		cin >> string_n;
 	}
-																												//Конец проверки вводимых символов
-	cout << "Строки: ";
+	//Конец проверки вводимых символов
+	cout << "Столбцы: ";
 	while (!(cin >> column_m)) {
 		cin.clear();
 		cin.ignore();
@@ -62,12 +62,12 @@ void inputB()
 	while (column_m < 1 || column_m > 100)																		// Цикл заставляющий ввести значения не меньше 1 и не больше 100
 	{
 		cout << "Недопустимая размерность матрицы, повтори ввод ещё раз" << endl;
-		cout << "Кол-во строк" << endl;
+		cout << "Кол-во столбцов" << endl;
 		cin >> column_m;
 	}
 }
 double** matrix_input(double** matrix1, int i, int j)
-{ 
+{
 	int n, m;
 	for (n = 0; n < i; n++)
 	{
@@ -81,18 +81,7 @@ double** matrix_input(double** matrix1, int i, int j)
 	}
 	return matrix1;
 }
-/*double** getMatrix1() {
-	double **matrix1 = new double *[string_x];
-	for (i = 0; i < string_x; i++)
-		matrix1[i] = new double[column_y];
-	return matrix1;
-}
-double** getMatrix2() {
-	double **matrix1 = new double *[string_x];
-	for (i = 0; i < string_x; i++)
-		matrix1[i] = new double[column_m];
-	return matrix1;
-} */
+
 double** getMatrix(int x, int y) {
 	double **matrix = new double *[x];
 	for (i = 0; i < x; i++)
@@ -100,21 +89,22 @@ double** getMatrix(int x, int y) {
 	return matrix;
 }
 
-int greater_value(int x, int y) {
-	if (x > y) {
-		return x;
-	}
-	else return y;
-	return x;
-}
 void output(double** matrix) {
-	 for (i = 0; i < string_x; i++)
-	 {
-		 for (j = 0; j < column_y; j++)
-			 printf("%-10g", matrix[i][j]);
-		 cout << endl;
-	 }
- }
+	for (i = 0; i < string_x; i++)
+	{
+		for (j = 0; j < column_y; j++)
+			printf("%-10g", matrix[i][j]);
+		cout << endl;
+	}
+}
+void output_multip(double** matrix) {
+	for (i = 0; i < string_n; i++)
+	{
+		for (j = 0; j < column_m; j++)
+			printf("%-10g", matrix[i][j]);
+		cout << endl;
+	}
+}
 void sum(double** matrix1, double** matrix2, double** matrix3) {
 	for (i = 0; i < string_x; i++)
 	{
@@ -122,7 +112,7 @@ void sum(double** matrix1, double** matrix2, double** matrix3) {
 		{
 			matrix3[i][j] = 0;
 			matrix3[i][j] = matrix1[i][j] + matrix2[i][j];
-			
+
 			printf("%-10g", matrix3[i][j]);
 		}
 		cout << endl;
@@ -141,14 +131,15 @@ void minys(double** matrix1, double** matrix2, double** matrix3) {
 	}
 }
 void multip(double** matrix1, double** matrix2, double** matrix3) {
-	for (i = 0; i < string_x; i++)
+	for (i = 0; i < string_n; i++)
 	{
-		for (j = 0; j < column_m; j++)
+		for (j = 0; j < column_y; j++)
 		{
 			matrix3[i][j] = 0;
-			for (int k = 0; k < string_x; k++)
+			for (int k = 0; k < string_n; k++)
 				matrix3[i][j] += matrix1[i][k] * matrix2[k][j];
-			printf("%-10g", matrix3[i][j]);
+			//	printf("%-20g", matrix3[i][j]);
+			cout << matrix3[i][j] << endl;
 		}
 		cout << endl;
 	}
@@ -169,7 +160,7 @@ int main()
 {
 	setlocale(LC_ALL, "Russian");																				// Изменение языка ввывода на русский
 	cout << "Здравствуйте, я помогу вам решить матрицу." << endl;												// Начало выбора выполняемой операции
-	int method;																									
+	int method;
 	cout << "Выберите действие которое вы хотите осуществить с матрицей." << endl << "Введите цифру (1) для сложения матриц, (2) вычитание, (3) умножение, (4) траспонирование." << endl;// Доступные операции
 	cin >> method;																								// Ввод номера операции
 	while (method < 1 || method > 4)																			// Цикл который заставляет выбрать одну из операций
@@ -179,12 +170,12 @@ int main()
 	}
 	if (method == 1) {																							// Начало операции сложения
 		cout << "Вы выбрали сложение матриц!" << endl;
-		cout << "Введите размерность матрицы A." << endl;														
+		cout << "Введите размерность матрицы A." << endl;
 		inputA();																								// Ввод размерности матрицы A, так же он используется и для матрицы B, так как при сложении матрицы должны быть одинаковыми.
 		double** matrix1 = matrix_input(getMatrix(string_x, column_y), string_x, column_y);						// Передача функции gerMatrix1() в которой уже инициализирован массив в функцию matrix_input. Результат matrix_input приравнивается к заданной переменной matrix1.
 		cout << "Матрица A:" << endl;
 		output(matrix1);																						// Вывод массива ввиде таблицы
-																												
+
 		cout << "Введите элементы матрицы B." << endl;
 		double** matrix2 = matrix_input(getMatrix(string_x, column_y), string_x, column_y);
 		cout << "Матрица B:" << endl;
@@ -225,16 +216,16 @@ int main()
 		inputB();																								// Ввод размерности матрицы B
 		double** matrix2 = matrix_input(getMatrix(string_n, column_m), string_n, column_m);						// Передача функции gerMatrix2() в которой уже инициализирован массив в функцию matrix_input. Результат matrix_input приравнивается к заданной переменной matrix2.
 		cout << "Матрица B:" << endl;
-		output(matrix2);																						// Вывод массива ввиде таблицы
+		output_multip(matrix2);																						// Вывод массива ввиде таблицы
 
 		cout << "Полученна матрица C:" << "C=A+B" << endl;
-		double** matrix3 = getMatrix(greater_value(string_x, string_n), greater_value(column_m, column_y));														// Инициализация новой массива matrix3
+		double** matrix3 = getMatrix(column_y, string_n);														// Инициализация новой массива matrix3
 		multip(matrix1, matrix2, matrix3);																		// Умножение матриц и вывод результата
-		
+
 	}
 
 	else {																										// Начало операции транспонирования
-		cout << "Вы выбрали транспонирование матрицы!" << endl;													
+		cout << "Вы выбрали транспонирование матрицы!" << endl;
 		cout << "Введите размерность матрицы A." << endl;
 		inputA();																								// Ввод размерности матрицы
 		double** matrix1 = matrix_input(getMatrix(string_x, column_y), string_x, column_y);						// Передача функции gerMatrix1() в которой уже инициализирован массив в функцию matrix_input. Результат matrix_input приравнивается к заданной переменной matrix1.
